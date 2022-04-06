d3bab2652cccd4974134f606fbc5a7806b1c1141
= <=>
2022-04-06 13:07:51 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="logot" />
+        <p>HELLO WORLD
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
