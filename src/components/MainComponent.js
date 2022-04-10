f02a7485df40e757a1a3e469976596ff33be490d
= <=>
2022-04-10 16:31:26 +0700

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import {Routes, Route} from 'react-router-dom';
 
 class Main extends Component{
     constructor(props){
@@ -16,14 +17,22 @@ class Main extends Component{
     onDishSelect(dishId){
     this.setState({selectedDish: dishId})}
     
-    render(){return (
+    render(){
+        const HomePage = () => {
+            return(
+                <Home 
+                />
+            );
+          }
+        
+        return (
         <div>
             <Header/>
+            <Routes>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
 
-            <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelect(dishId)}/>
-
-            <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]} />
-
+            </Routes>
             <Footer />
         </div>
     )}
@@ -33,3 +42,5 @@ export default Main;
 //MainComponent.js đóng vai trò container component để hiển thị Navbar, Menu và DishDetail
 //<Menu /> sử dụng sự kiện onClick để gán giá trị selectedDish = id của món ăn
 // <DishDetail/> filter các mảng món ăn trong CSDL, tìm mảng có id món ăn =selectedDish thì trả về thành props để truy xuất tại DishDetailComponent.js
+
+//Switch trong react-router v6 đã đổi tên thành Routes
