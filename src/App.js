eee7ee1f5264777efe3fabe054c0dda9c9a9a1f2
= <=>
2022-04-18 00:04:07 +0700

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter } from 'react-router-dom';
 import "./App.css";
 import Main from './components/MainComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
 class App extends Component{
 
   
   render(){
     return (
-      <BrowserRouter>
-        <div>
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
