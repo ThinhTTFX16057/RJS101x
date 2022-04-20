dd6d3f13a1e34d8633e926f32e91d1f204e3ff35
= <=>
2022-04-20 13:41:08 +0700

    Redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import {Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Button}from 'reactstrap';
 import {Link} from 'react-router-dom';
 import CommentForm from './CommentForm';
+import {Loading} from './LoadingComponent';
+
 
 function RenderDish({x}){return(
     <Card key={x.id} className='col-12 col-md-5 m-1'>
@@ -38,7 +40,25 @@ function RenderComments({comments,addComment,dishId}){
 } 
 
 const DishDetail=(props)=>{
-    if (props.dish!=null){return(
+    if (props.isLoading){
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading/>
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess){
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish!=null){return(
         <div className="container">
             <div className='row'>
                 <Breadcrumb>
